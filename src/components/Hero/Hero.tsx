903975942916eea2b3c506ee98ac35ee9abbfee6
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-05-19 13:38:59 -0300

    feat: Finalização do componente Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,21 +4,25 @@ export function Hero() {
   return (
     <main className={styles.hero}>
       <div className={styles.left}>
-        <div className={styles.containerImg}>
-          <img src={profileImg} alt="" />
-        </div>
-      </div>
-      <div className={styles.right}>
         <h1>Francisco Lucas Barbosa</h1>
         <h2>Desenvolvedor Full-stack</h2>
         <p>
           Sou apaixonado por tecnologia e desenvolvimento web. Busco criar
           soluções modernas, eficientes e com foco na experiência do usuário.
         </p>
-
+        <p>
+          Domino tecnologias como JavaScript, TypeScript, Angular, React, Vue,
+          Java, Spring Boot, PHP, Laravel e SQL, desenvolvendo soluções
+          completas do front-end ao back-end.
+        </p>
         <div className={styles.buttons}>
           <button>Contato</button>
           <button>Download CV</button>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.containerImg}>
+          <img src={profileImg} alt="" />
         </div>
       </div>
     </main>
